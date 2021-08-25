216a91cd59e632719f9c61e29098c7f850b1cb80
Axleonr <axleonr@aragon.unam.mx>
2021-08-25 18:19:45 -0500

    Se añade el código del problema 2

--- PASE_Práctica1.R
+++ PASE_Práctica1.R
@@ -17,3 +17,21 @@ for(i in 1:40){
     break
   }
 }
+# Problema 2
+
+simpson <- function(fun, a, b, n=10000) {
+  h <- (b-a)/n
+  x <- seq(a, b, by=h)
+  if (n == 2) {
+    s <- fun(x[1]) + 4*fun(x[2]) +fun(x[3])
+  } else {
+    s <- fun(x[1]) + fun(x[n+1]) + 2*sum(fun(x[seq(2,n-1,by=2)])) + 4 *sum(fun(x[seq(1,n, by=2)]))
+  }
+  s <- s*h/3
+  return(s)
+}
+
+cuadrado <- function(x) x^2
+seno <- function(x) sin(x)
+ealamenos <- function(x) exp(-x**2)
+raiz <- function(x) sqrt(1+x**3)
