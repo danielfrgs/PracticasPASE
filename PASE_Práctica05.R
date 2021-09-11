3a130a4cc6f2676c8a28ebf3032d4fd537740487
Daniel <61167537+danielfrgs@users.noreply.github.com>
2021-09-10 19:36:55 -0500

    Update PASE_Práctica05.R
    xd

--- PASE_Práctica05.R
+++ PASE_Práctica05.R
@@ -39,7 +39,29 @@ plot_pareto <- function(a, b, N){
 plot_weibull(1, 5, 50000)
 plot_pareto(10, 3, 50000)
 
+# Histograma Weibull(1,5),
 
+# Funcion de distribución Weibull
+f1 <- function(x, k, lamda){
+  (k/lamda)*((x/lamda)^(k-1))*exp(-(x/lamda)^k)
+}
+
+hist(Weibull(1, 5, 100000), breaks = 100, probability = TRUE)
+x1 <- seq(0, 50, by=0.01)
+lines(x1, f1(x1, 1, 5), col = 'blue')
+
+# Histograma Pareto
+
+# Función de distribución Pareto
+f2 <- function(x, a, b){
+  (a*b^a)/((b+x)^(a+1))
+}
+
+par(new = TRUE, lines(x2, f(x2, 10, 3), col = 'blue'))
+x2 <- seq(0, 5, by=0.01)
+lines(x2, f2(x2, 10, 3), col = 'blue')
+
+#####
 
 # Pareto(10,3)
 # X = ((b)(1-(1-u)^(1/a)))/((1-u)^(1/a))
